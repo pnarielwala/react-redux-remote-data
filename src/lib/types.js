466664f10eab39331c1defa0664322d7aec75dcc
Parth Narielwala <parth.narielwala@trunkclub.com>
2019-02-05 11:48:44 -0600

    Fix type

--- src/lib/types.js
+++ src/lib/types.js
@@ -1,9 +1,9 @@
 // @flow
-export type RemoteDataT<D, E> =
-  | { phase: 'NOT_ASKED' }
-  | { phase: 'PENDING' }
-  | { phase: 'SUCCESS', data: D }
-  | { phase: 'FAILURE', error: ?E }
+export type RemoteDataT<D, E, M = any> =
+  | { phase: 'NOT_ASKED', meta?: M }
+  | { phase: 'PENDING', meta?: M }
+  | { phase: 'SUCCESS', data: D, meta?: M }
+  | { phase: 'FAILURE', error: ?E, meta?: M }
 
 export type StateT = RemoteDataT<*, *>
 
